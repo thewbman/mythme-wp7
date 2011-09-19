f7aa69e74c0eb364a851fff6a9d8c10aaf1a293f
thewbman <thewbman@0bfcf3fa-9bd8-e540-5558-64daa32b7f51>
2011-09-19 03:56:34 +0000

    Initial check-in

---
+++ branches/MythMe/MainPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace MythMe
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+
+                if (App.ViewModel.appSettings.FirstRunSetting) MessageBox.Show("Welcome to MythMe.", "MythMe", MessageBoxButton.OK);
+
+                App.ViewModel.appSettings.FirstRunSetting = false;
+
+            }
+            else
+            {
+                App.ViewModel.Connected = false;
+            }
+
+
+            MasterBackendTitle.Text = App.ViewModel.appSettings.MasterBackendIpSetting;
+        }
+
+        private void remoteButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+        }
+
+        private void recordedButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+
+        private void upcomingButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+
+        private void guideButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+
+        private void searchButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+
+        private void videosButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+
+        private void musicButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+
+        private void statusButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+
+        private void logButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+
+        private void preferencesButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+
+        private void helpButton_Tap(object sender, GestureEventArgs e)
+        {
+            MessageBox.Show("Not yet implimented");
+
+        }
+    }
+}
---
+++ branches/MythMe/ViewModels/FrontendsViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MythMe
+{
+    public class FrontendsViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        private string _host;
+        public string Host
+        {
+            get { return _host; }
+            set
+            {
+                if (value != _host)
+                {
+                    _host = value;
+                    NotifyPropertyChanged("Host");
+                }
+            }
+        }
+
+        private int _port;
+        public int Port
+        {
+            get { return _port; }
+            set
+            {
+                if (value != _port)
+                {
+                    _port = value;
+                    NotifyPropertyChanged("Port");
+                }
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+
