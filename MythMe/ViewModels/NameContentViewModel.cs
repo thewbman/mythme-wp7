524aef4728777158018d483fd680468c8e04f142
thewbman <thewbman@0bfcf3fa-9bd8-e540-5558-64daa32b7f51>
2011-09-28 06:39:04 +0000

    Add status; improve upcoming socket connection

---
+++ MythMe/ViewModels/NameContentViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MythMe
+{
+    public class NameContentViewModel : INotifyPropertyChanged
+    {
+        private string _Name;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                if (value != _Name)
+                {
+                    _Name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        private string _Content;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string Content
+        {
+            get
+            {
+                return _Content;
+            }
+            set
+            {
+                if (value != _Content)
+                {
+                    _Content = value;
+                    NotifyPropertyChanged("Content");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
